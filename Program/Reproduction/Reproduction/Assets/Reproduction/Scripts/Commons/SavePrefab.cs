3887ce50cb3629afdadb71645ddc3eef7c3147f6
Show Koudou <mail.show.Kondou@gmail.com>
2017-02-07 02:09:37 +0900

    プロジェクト立ち上げ

---
+++ Program/Reproduction/Reproduction/Assets/Reproduction/Scripts/Commons/SavePrefab.cs
@@ -0,0 +1,24 @@
+﻿
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+//
+//  [SavePrefab]
+//
+//  ファイル名：SavePrefab.cs
+//  説　　　明：プレハブの任意保存コマンド拡張
+//  制　作　者：Show Kondou
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  - 更新履歴 -
+//  2017  01/28　… 新規作成	※変更禁止
+//
+//_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+public class Util : Editor {
+	[UnityEditor.MenuItem( "Edit/SavePrefab %&s" )]
+	static void SavePrefab() {
+		AssetDatabase.SaveAssets();
+	}
+}
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Reproduction-
+### Reproduction
+# Show Kondou
